6c0d7d846bbe137011f9af7d5c7cb5c71efb5377
FredesAcosta <Acostafredes071@gmail.com>
2025-12-14 21:11:30 -0500

    Ultimos retoques

--- test.py
+++
@@ -1,3 +0,0 @@
-import bcrypt
-print(bcrypt.hashpw('Admin123*'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'))
-print(bcrypt.hashpw('Empleado123*'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'))
